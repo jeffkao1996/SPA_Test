5d4c388001ee2fa1d635e1b0874821121b102c8b
jeffkao1996 <62791878+jeffkao1996@users.noreply.github.com>
2025-09-26 15:38:27 +0800

    Update webpack.config.js
    調整為 絕對路徑

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './public/index.html', // 告訴插件你的 HTML 模板在哪裡
+            template: path.resolve(__dirname, 'public/index.html'), 
             filename: 'index.html',
             inject: 'body',
         })
