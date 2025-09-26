1bb0f81ab52c98c7f3e5ca8d70608756c75aae76
jeffkao1996 <62791878+jeffkao1996@users.noreply.github.com>
2025-09-26 15:08:40 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.js',
-        publicPath: './',
+        publicPath: '/',
     },
     resolve: {
         extensions: ['.js', '.jsx'],
